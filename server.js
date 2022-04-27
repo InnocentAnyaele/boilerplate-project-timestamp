6f4ad1667fcfb8af612140717587d1d08f80491a
Innocent <55434969+InnocentAnyaele@users.noreply.github.com>
2022-04-27 07:34:18 +0000

    update

--- server.js
+++ server.js
@@ -8,25 +8,51 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function(req, res) {
+
+  let input = req.params.date
+
+  if (!input) {
+    date = new Date()
+    return res.json({
+      'unix': date.getTime(),
+      'utc': date.toUTCString(),
+    })
+  }
+  if ( /^\d+$/.test(input)) {
+    let dateObj = new Date(parseInt(input))
+    return res.json({
+      'unix': dateObj.getTime(),
+      'utc': dateObj.toUTCString()
+    })
+  }
+  
+  if (new Date(input).toString() === 'Invalid Date') {
+    return res.json({ error: "Invalid Date" })
+  }
+  if (new Date(input)) {
+    return res.json({
+      'unix': new Date(input).getTime(),
+      'utc': new Date(input).toUTCString()
+    })
+  }
+
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
